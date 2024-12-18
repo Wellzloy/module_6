1518ddf963cab0652947e5a281a015e2a5595849
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-12-18 17:33:13 +0400

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,12 +1,15 @@
+import random
+
+
 class Animal:
     live = True
-    sound = None
-    _DEGREE_OF_DANGER = 0
+    sound = None #звук (изначально отсутствует)
+    _DEGREE_OF_DANGER = 0 #степень опасности существа
 
     def __init__(self, speed):
-        self.speed = speed
-        self._cords = [0, 0, 0]
-        
+        self.speed = speed #скорость передвижения существа (определяется при создании объекта)
+        self._cords = [0, 0, 0] #координаты в пространстве.
+
     def move(self, dx, dy, dz):
         self._cords[0] += dx * self.speed
         self._cords[1] += dy * self.speed
@@ -18,34 +21,46 @@ class Animal:
     def get_cords(self):
         print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
 
+    def attack(self):
+        if self._DEGREE_OF_DANGER > 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
+    def speak(self):
+        print(self.sound)
 
 
+class Bird(Animal): # Класс описывающий птиц.
+    beak = True # наличие клюва
 
-class Bird(Animal):
-    pass
+    def lay_eggs(self):
+        print(f'Here are(is) {random.randint(1, 4)} eggs for you')
 
 
-class AquaticAnimal(Animal):
-    pass
+class AquaticAnimal(Animal): # Класс описывающий плавающего животного.
+    _DEGREE_OF_DANGER = 3
 
-class PoisonousAnimal(Animal):
-    pass
+    def dive_in(self, dz):
+        self._cords[2] = -(abs(dz) * self.speed / 2)
+
+
+class PoisonousAnimal(Animal):  #Класс описывающий ядовитых животных
+    _DEGREE_OF_DANGER = 8
 
 
 class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
     sound = "Click-click-click"
 
 
-
 db = Duckbill(10)
 print(db.live)
 
-# print(db.beak)
-# db.speak()
-# db.attack()
+print(db.beak)
+db.speak()
+db.attack()
 db.move(1, 2, 3)
 db.get_cords()
-# db.dive_in(6)
-# db.get_cords()
-# db.lay_eggs()
-
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
